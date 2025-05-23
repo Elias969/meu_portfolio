8fdbb5399e0f2840c8711285edc156762121dee1
Elias969 <93712140+Elias969@users.noreply.github.com>
2025-05-23 12:11:30 -0300

    Update Feedback.js

--- meu-portfolio/src/components/Feedback.js
+++ meu-portfolio/src/components/Feedback.js
@@ -69,12 +69,7 @@ export default function Feedback() {
         </button>
       </form>
 
-        <div
-    className="space-y-4 mt-10 overflow-hidden"
-    onMouseEnter={() => setIsPaused(true)}
-    onMouseLeave={() => setIsPaused(false)}
-  >
-
+       <div className="space-y-4 mt-10 overflow-hidden">
         {[0, 1].map((row) => (
           <div key={row} className="whitespace-nowrap animate-scroll">
             {feedbacks
